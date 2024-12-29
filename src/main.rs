918575fdc8de3f819de0ee529a6657a9b35caa40
Alexey <alexvoit70@gmail.com>
2024-12-29 23:02:47 +0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use std::thread::{self, spawn};
+
+fn main() {
+    let t1 = thread::spawn(func);
+    let t2 = thread::spawn(func);
+    println!("Hello fron the main thread");
+    t1.join().unwrap();
+    t2.join().unwrap();
+
+    let numbers = vec![1, 2, 3];
+    spawn(move || {
+        for n in &numbers {
+            println!("{n}");
+        }
+    }).join().unwrap();
+
+    let numbers = Vec::from_iter(0..=100);
+    let t = thread::spawn(move || {
+        let len = numbers.len();
+        let sum = numbers.iter().sum::<usize>();
+        sum / len
+    });
+
+    let average = t.join().unwrap();
+    println!("average: {average}");
+    thread::Builder::new().spawn(func).unwrap().join().unwrap();
+
+    let mut numbers = vec![1, 2, 3];
+    thread::scope(|s| {
+        s.spawn(|| {
+            numbers.push(1);
+            println!("length: {}", numbers.len());
+        });
+        s.spawn(|| {
+            numbers.push(2);
+            for n in &numbers {
+                println!("{n}");
+            }
+        });
+    });
+}
+
+fn func() {
+    println!("Hello from another thread!");
+    let id = thread::current().id();
+    println!("This is my thread id: {id:?}");
+}
